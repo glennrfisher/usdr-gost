365a3261e436b2f1d343e53f0a30efa990ff9e0b
David Kay <dkay@advisr.com>
2022-06-23 14:42:50 -0500

    chore: adjusting console logs

--- packages/server/src/routes/grants.js
+++ packages/server/src/routes/grants.js
@@ -51,7 +51,7 @@ function getAwardFloor(grant) {
 }
 
 router.get('/', requireUser, async (req, res) => {
-    console.log('im here');
+    console.log('get grants');
     let agencyCriteria;
     // if we want interested, assigned, grants for a user, do not filter by eligibility or keywords
     if (!req.query.interestedByMe && !req.query.assignedToAgency) {
@@ -68,7 +68,7 @@ router.get('/', requireUser, async (req, res) => {
             assignedToAgency: req.query.assignedToAgency ? req.query.assignedToAgency : null,
         },
     });
-    console.log('im here');
+    console.log('grants fetched');
     res.json(grants);
 });
 
