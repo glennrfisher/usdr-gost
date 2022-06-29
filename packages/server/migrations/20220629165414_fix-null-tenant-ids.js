7ff07cfc3ffd0b3d2cf76ebc5445225648e0c89a
David Kay <dkay@advisr.com>
2022-06-29 12:48:25 -0500

    chore: updated migration for tenants

--- packages/server/migrations/20220629165414_fix-null-tenant-ids.js
+++ packages/server/migrations/20220629165414_fix-null-tenant-ids.js
@@ -1,20 +1,21 @@
 // eslint-disable-next-line func-names
 exports.up = async function (knex) {
     return knex.schema
+        .raw(`update agencies set tenant_id = tenants.id FROM tenants where tenants.display_name = 'State of Nevada Test' and agencies.tenant_id is null`)
         .alterTable('agencies', (table) => {
-            table.integer('tenant_id').unsigned().notNullable();
+            table.integer('tenant_id').unsigned().notNullable().alter();
         })
-        .raw(`update agencies set agencies.tenant_id = tenant.id from tenant where tenant.name = 'State of Nevada Test' and tenant_id is null`)
+        .raw(`update users set tenant_id = tenants.id FROM tenants where tenants.display_name = 'State of Nevada Test' and users.tenant_id is null`)
         .alterTable('users', (table) => {
-            table.integer('tenant_id').unsigned().notNullable();
-        }).raw(`update users set users.tenant_id = tenant.id from tenant where tenant.name = 'State of Nevada Test' and tenant_id is null`);
+            table.integer('tenant_id').unsigned().notNullable().alter();
+        });
 };
 exports.down = async function (knex) {
     return knex.schema
         .alterTable('agencies', (table) => {
-            table.integer('tenant_id').unsigned().nullable();
+            table.integer('tenant_id').unsigned().nullable().alter();
         })
         .alterTable('users', (table) => {
-            table.integer('tenant_id').unsigned().nullable();
+            table.integer('tenant_id').unsigned().nullable().alter();
         });
 };
