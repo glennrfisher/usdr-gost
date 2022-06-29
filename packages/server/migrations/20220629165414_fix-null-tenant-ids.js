4d3ec1ff718546fc52188441c5eaf31c0814c612
David Kay <dkay@advisr.com>
2022-06-29 12:33:09 -0500

    chore: added null tenants migration

--- packages/server/migrations/20220629165414_fix-null-tenant-ids.js
+++ packages/server/migrations/20220629165414_fix-null-tenant-ids.js
@@ -1,3 +1,20 @@
 // eslint-disable-next-line func-names
-// exports.up = async function (knex) {};
-// exports.down = async function (knex) {};
+exports.up = async function (knex) {
+    return knex.schema
+        .alterTable('agencies', (table) => {
+            table.integer('tenant_id').unsigned().notNullable();
+        })
+        .raw(`update agencies set agencies.tenant_id = tenant.id from tenant where tenant.name = 'State of Nevada Test' and tenant_id is null`)
+        .alterTable('users', (table) => {
+            table.integer('tenant_id').unsigned().notNullable();
+        }).raw(`update users set users.tenant_id = tenant.id from tenant where tenant.name = 'State of Nevada Test' and tenant_id is null`);
+};
+exports.down = async function (knex) {
+    return knex.schema
+        .alterTable('agencies', (table) => {
+            table.integer('tenant_id').unsigned().nullable();
+        })
+        .alterTable('users', (table) => {
+            table.integer('tenant_id').unsigned().nullable();
+        });
+};
