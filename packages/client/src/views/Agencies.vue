1f425c6058042b5254daee5c25daab8e61714294
David Kay <dkay@advisr.com>
2022-07-06 14:52:46 -0500

    fix: formatting

--- packages/client/src/views/Agencies.vue
+++ packages/client/src/views/Agencies.vue
@@ -1,40 +1,46 @@
 <template>
-<section class="container-fluid">
-  <b-row>
-    <b-col><h2>Agencies</h2></b-col>
-    <b-col></b-col>
-    <b-col class="d-flex justify-content-end">
-      <div>
-        <b-button variant="success" @click="openAddAgencyModal">Add</b-button>
-      </div>
-    </b-col>
-  </b-row>
-  <b-table sticky-header="600px" hover :items="formattedTenantAgencies" :fields="fields">
-    <template #cell(name)="row">
-        {{row.item.name}}
+  <section class="container-fluid">
+    <b-row>
+      <b-col><h2>Agencies</h2></b-col>
+      <b-col></b-col>
+      <b-col class="d-flex justify-content-end">
+        <div>
+          <b-button variant="success" @click="openAddAgencyModal">Add</b-button>
+        </div>
+      </b-col>
+    </b-row>
+    <b-table
+      sticky-header="600px"
+      hover
+      :items="formattedTenantAgencies"
+      :fields="fields"
+    >
+      <template #cell(name)="row">
+        {{ row.item.name }}
       </template>
       <template #cell(warning_threshold)="row">
-        {{row.item.warning_threshold}} days
+        {{ row.item.warning_threshold }} days
       </template>
       <template #cell(danger_threshold)="row">
-        {{row.item.danger_threshold}} days
+        {{ row.item.danger_threshold }} days
       </template>
       <template #cell(actions)="row">
-      <b-button v-if="userRole === 'admin'" class="mr-1" size="sm" @click="openEditAgencyModal(row.item)">
-        <b-icon icon="pencil-fill" aria-hidden="true"></b-icon>
-      </b-button>
-    </template>
-  </b-table>
-  <EditAgencyModal
-     :agency.sync="editingAgency"
-  />
-  <AddAgencyModal
-  :showDialog.sync="showAddAgencyModal"/>
-</section>
+        <b-button
+          v-if="userRole === 'admin'"
+          class="mr-1"
+          size="sm"
+          @click="openEditAgencyModal(row.item)"
+        >
+          <b-icon icon="pencil-fill" aria-hidden="true"></b-icon>
+        </b-button>
+      </template>
+    </b-table>
+    <EditAgencyModal :agency.sync="editingAgency" />
+    <AddAgencyModal :showDialog.sync="showAddAgencyModal" />
+  </section>
 </template>
 
 <script>
-
 import { mapActions, mapGetters } from 'vuex';
 
 import EditAgencyModal from '@/components/Modals/EditAgency.vue';
