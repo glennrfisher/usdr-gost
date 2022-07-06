b8061628525a1f5dce89d28b6da7cd8fab8cb245
David Kay <dkay@advisr.com>
2022-07-06 14:51:51 -0500

    fix: improve deleteagency call

--- packages/client/src/components/Modals/EditAgency.vue
+++ packages/client/src/components/Modals/EditAgency.vue
@@ -174,14 +174,15 @@ export default {
         'Are you sure you want to delete this agency? This cannot be undone. If the agency has children,'
       + ' reassign child agencies to continue deletion.',
       ).then(() => {
-        this.deleteAgency({
-          agencyId: this.agency.id,
-          parent: this.agency.parent,
-          name: this.agency.name,
-          abbreviation: this.agency.abbreviation,
-          warningThreshold: this.formData.warningThreshold,
-          dangerThreshold: this.formData.dangerThreshold,
-        });
+        this.deleteAgency(this.agency.id);
+        // this.deleteAgency({
+        //   agencyId: this.agency.id,
+        //   parent: this.agency.parent,
+        //   name: this.agency.name,
+        //   abbreviation: this.agency.abbreviation,
+        //   warningThreshold: this.formData.warningThreshold,
+        //   dangerThreshold: this.formData.dangerThreshold,
+        // });
         this.resetModal();
       })
         .catch((err) => {
